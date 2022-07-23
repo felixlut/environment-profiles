96a9bd6a6428859265f04800b4d32e793d6c1f6a
Felix <felix.luthman@gmail.com>
2022-07-23 13:03:58 +0200

    scoop tools install script + move windows stuff to own folder

---
+++ Windows/ScoopInstallTools.ps1
@@ -0,0 +1,5 @@
+# Imports buckets and tools from scoop
+
+# scoop export > scoopfile.json (https://github.com/ScoopInstaller/Scoop/blob/80b52e32a11fda2089949aaec00295d6185da3f7/libexec/scoop-export.ps1)
+
+scoop import "https://github.com/felixlut/environment-profiles/blob/main//Windows/ScoopInstallTools.json"
